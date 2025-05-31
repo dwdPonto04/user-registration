e3fef4dd83b4c087ca151586a7a9776e99f6aa18
David <davidfl44.pro@gmail.com>
2025-05-30 22:55:21 -0300

    Update UserService

--- src/main/java/com/devsuperior/user_registration/services/UserService.java
+++ src/main/java/com/devsuperior/user_registration/services/UserService.java
@@ -1,10 +1,10 @@
 package com.devsuperior.user_registration.services;
-
 import com.devsuperior.user_registration.dto.UserDTO;
 import com.devsuperior.user_registration.repositories.UserRepository;
 import com.devsuperior.user_registration.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -12,6 +12,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public UserDTO findById(Long id){
        User entity = userRepository.findById(id).get();
        UserDTO dto = new UserDTO(entity);
