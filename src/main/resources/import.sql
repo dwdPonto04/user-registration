1a468bca3238dc1b435a88626d27e15af88b07f9
David <davidfl44.pro@gmail.com>
2025-05-30 22:42:35 -0300

    User seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_user (name , password) VALUES ('Maria', 'asdaslkdjak454');
+INSERT INTO tb_user (name , password) VALUES ('Marcia', 'as5645454');
+INSERT INTO tb_user (name , password) VALUES ('João', 'as56454547888');
+INSERT INTO tb_user (name , password) VALUES ('Breno', '44as5sas');
+INSERT INTO tb_user (name , password) VALUES ('Abrãao', 'as44789');
+INSERT INTO tb_user (name , password) VALUES ('Denilson', 'asdaqwy455');
+INSERT INTO tb_user (name , password) VALUES ('Anizío', 'agyyt1556');
+
